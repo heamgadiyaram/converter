a8db66a76a9e547192d19319320dc7a7a4e6b3f5
Heam Gadiyaram <heam.gadiyaram07@gmail.com>
2025-03-12 12:40:38 -0400

    added frontend

--- src/App.js
+++ src/App.js
@@ -1,23 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
 
 function App() {
+  const [inputValue, setInputValue] = useState('');
+  const [logs, setLogs] = useState([]);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const response = await fetch('http://localhost:8000/convert', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ inputValue: inputValue }),
+      });
+      setLogs((prevLogs) => [...prevLogs, `Response Status: ${response.status}`]);
+
+
+      if (!response.ok) {
+          throw new Error('Network response was not ok');
+      }
+
+      const data = await response.json();
+      console.log('Response from backend:', data);
+      setLogs((prevLogs) => [...prevLogs, `Response from backend: ${JSON.stringify(data)}`]);
+      setLogs((prevLogs) => [...prevLogs, `Your Spotify Link: ${data.link}`]);
+
+  } catch (error) {
+    console.error('There was an error sending the data:', error);
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+    <form onSubmit={handleSubmit}>
+      <input
+        type="text"
+        value={inputValue}
+        onChange={(e) => setInputValue(e.target.value)}
+      />
+      <button type="submit">Submit</button>
+    </form>
+
+    <div>
+        {logs.map((log, index) => (
+          <p key={index}>{log}</p>
+        ))}
+    </div>
     </div>
   );
 }
